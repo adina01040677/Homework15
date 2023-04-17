5cb35bddfe3b8ae7d6d3c7a33a39e67a23d4b7bb
адина <adina.atakulova04@gmail.om>
2023-04-17 16:32:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ *Circle deген класс тузунуз, анын PI деген свойствасы, area
+ * жана circumference деген статик методдору болсун.
+ * areaны табуу учун: PI * (радиус * радиус)
+ * окружность табуу учун PI * 2 * радиус деген формулалар колдонулат*/
+public class Main {
+    public static void main(String[] args) {
+        new Circle(3);
+       Circle.area();
+       Circle.cumfcirerence();
+    }
+}
